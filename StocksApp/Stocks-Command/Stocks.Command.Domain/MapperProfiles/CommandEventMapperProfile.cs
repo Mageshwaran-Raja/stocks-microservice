82193819c55de8656b9b4890758f3aff433f6217
mageshwaran16 <mageshwaran.raja@kanini.com>
2023-08-26 22:24:44 +0530

    Stocks Aggregate

---
+++ StocksApp/Stocks-Command/Stocks.Command.Domain/MapperProfiles/CommandEventMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Stocks.Common.DTOs;
+using Stocks.Common.Events;
+
+namespace Stocks.Command.Domain.MapperProfiles
+{
+    public class CommandEventMapperProfile : Profile
+    {
+        public CommandEventMapperProfile() 
+        {
+            CreateMap<CreateStockDTO, StockCreatedEvent>();
+        }
+    }
+}
