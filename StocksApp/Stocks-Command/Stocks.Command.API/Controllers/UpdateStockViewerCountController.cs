d06b75c2c7d7cfab035c696d1d95340734a9a552
mageshwaran16 <mageshwaran.raja@kanini.com>
2023-08-26 23:02:49 +0530

    Add & Update Stock Controllers

--- StocksApp/Stocks-Command/Stocks.Command.API/Controllers/UpdateStockViewerCountController.cs
+++ StocksApp/Stocks-Command/Stocks.Command.API/Controllers/UpdateStockViewerCountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CQRS.Core.Exceptions;
+using CQRS.Core.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Stocks.Command.API.Commands;
+using Stocks.Common.DTOs;
 
 namespace Stocks.Command.API.Controllers
 {
@@ -7,5 +10,54 @@ namespace Stocks.Command.API.Controllers
     [ApiController]
     public class UpdateStockViewerCountController : ControllerBase
     {
+        private readonly ILogger<UpdateStockViewerCountController> _logger;
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public UpdateStockViewerCountController(ILogger<UpdateStockViewerCountController> logger,
+            ICommandDispatcher commandDispatcher)
+        {
+            _logger = logger;
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> LikePostAsync(Guid id)
+        {
+            try
+            {
+                await _commandDispatcher.SendAsync(new UpdateStockViewerCountCommand { Id = id });
+
+                return Ok(new BaseResponse
+                {
+                    Message = "Update Stock Viewer Count Request Completed Successfully!"
+                });
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Client made a bad request!");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (AggregateNotFoundException ex)
+            {
+                _logger.Log(LogLevel.Warning, ex, "Cannot retrieve the aggregate! Client Send a wrong Id");
+                return BadRequest(new BaseResponse
+                {
+                    Message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error while processing request";
+                _logger.Log(LogLevel.Error, ex, SAFE_ERROR_MESSAGE);
+
+                return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+                {
+                    Message = SAFE_ERROR_MESSAGE
+                });
+            }
+        }
     }
 }
